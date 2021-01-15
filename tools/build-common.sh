26ba1a8b75aa9a16b47ca60932442dba56c63614
naiba <hi@nai.ba>
2021-01-15 20:48:01 +0800

    log

--- tools/build-common.sh
+++ tools/build-common.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -x
 export PLATFORM_TYPE=""
 export PKG_CONFIG_PATH=$(which pkg-config)
 
